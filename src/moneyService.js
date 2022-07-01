86c32cba9933a3717c17fa19e878fbf04d699257
Caroline Cerussi <caroceru@gmail.com>
2022-07-01 10:49:37 -0700

    add postman tested url from exchangerate api source

---
+++ src/moneyService.js
@@ -0,0 +1,13 @@
+//  inputted codes that are paired in url and entered amount will return conversion//
+//if error- entered an amount less than or equal to zero- alert please enter valid amount//
+
+const url =
+
+
+//money code USA 
+
+// CAD CANADIAN DOLLAR CANADA
+//EUR EURO EUROPEAN UNION
+//HKD HONK KONG DOLLAR HONG KONG
+//JPY JAPANESE YEN  JAPAN
+//MXN MEXICAN PESO MEXICO
